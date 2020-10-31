0c25030e161214f7edc8b5eb53a2c09491dbed34
mizukenwater <mizuken.water@gmail.com>
2020-10-31 21:08:31 +0900

    Modify

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ cd build
 docker-compose run web rails new ../src --force --no-deps --database=mysql --skip-test --api
 cp database.yml ../src/config/database.yml
 docker-compose build
+docker-compose up
